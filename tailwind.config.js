5099b0ffc7df513876beeab0f10d3973e04fa0c5
alyreza <alireza.azizii.1386@gmail.com>
2024-10-06 17:48:21 +0330

    fit: header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    colors: {
+      black: "#111111",
+      white: "#ffffff",
+      cream: "#C2B4A3", 
+      dark: "#1A1A1A",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+        sm: '2rem',
+        lg: '2rem',
+        xl: '5rem',
+        '2xl': '1rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
