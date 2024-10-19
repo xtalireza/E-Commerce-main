fd2ac16819b5f83c115051061d30018d55a626b9
alyreza <alireza.azizii.1386@gmail.com>
2024-10-19 19:41:42 +0330

    fit: aded div wardrobe

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       cream: "#C2B4A3", 
       dark: "#1A1A1A",
       gray:"#81807E",
-      paleBlue: "#B3B3B2",
+      paleGray: "#B3B3B2",
       grey: "#B3B3B2",
       transparent: "transparent",
     },
